d0360cf7005b826fd840ee8016c82bc90d881d23
Travis White <travis.white0101@gmail.com>
2026-05-27 09:54:44 -0400

    Update styling for Recent Games component
    - Provide title-text and std-btn-width global styling rules to text and
      button elements within component
    - Override the Carousel Item element in the Recent Games component
      to match Steam-like theme via its background color and border
    - Remove component container border
    - Adjust alignment and spacing between game cards
    - Update sizing of elements to be based off of viewport percentage to
      allow for greater scalability between various resolutions and aspect
      ratios
    - Remove previously implemented CarouselResizeDirective, and its related
      methods and variables within the Recent Games component, in favor of
      implementing a direct array of CarouselResponsiveOptions within the
      Recent Games component for dynamically updating the number of carousel
      items visible within a given period based on the viewport size

--- src/app/container/components/dashboard/profile/recent-games/recent-games.ts
+++ src/app/container/components/dashboard/profile/recent-games/recent-games.ts
@@ -1,38 +1,14 @@
 import { DatePipe, NgOptimizedImage } from '@angular/common'
-import type { AfterViewInit, ElementRef, OnDestroy, OnInit } from '@angular/core'
-import { booleanAttribute, ChangeDetectorRef, Component, Directive, inject, Input, ViewChild } from '@angular/core'
+import type { OnDestroy, OnInit } from '@angular/core'
+import { booleanAttribute, Component, inject, Input } from '@angular/core'
 import { MatCardModule } from '@angular/material/card'
 import { MatDialog, MatDialogConfig } from '@angular/material/dialog'
-import type { Carousel, CarouselResponsiveOptions } from 'primeng/carousel'
+import type { CarouselResponsiveOptions } from 'primeng/carousel'
 import { CarouselModule } from 'primeng/carousel'
 import { ImageFallback } from '../../../../../directives/image-fallback/image-fallback'
 import type { IUser, IUserGameInfo } from '../../../../../models/Steam'
 import { StateService } from '../../../../../services/state/state-service'
 import { GameDialog } from '../../../shared/game-dialog/game-dialog'
-
-@Directive({
-  standalone: true
-})
-export class CarouselResizeDirective {
-  protected numVisible = 3
-  protected numScroll = 1
-  private resizeObserver!: ResizeObserver
-
-  private updateCarouselConfig (containerWidth: number): void {
-    if (containerWidth < 950) {
-      this.numVisible = 1
-      this.numScroll = 1
-    }
-    else if (containerWidth < 1500) {
-      this.numVisible = 2
-      this.numScroll = 2
-    }
-    else {
-      this.numVisible = 3
-      this.numScroll = 3
-    }
-  }
-}
 
 @Component({
   selector: 'app-recent-games',
@@ -46,21 +22,33 @@ export class CarouselResizeDirective {
   templateUrl: './recent-games.html',
   styleUrl: './recent-games.scss'
 })
-export class RecentGames implements OnInit, AfterViewInit, OnDestroy {
+export class RecentGames implements OnInit, OnDestroy {
   //Dependency Injections
   private readonly state: StateService = inject(StateService)
   private readonly dialog: MatDialog = inject(MatDialog)
-  private readonly cdr: ChangeDetectorRef = inject(ChangeDetectorRef)
 
   @Input({ required: true }) public recentlyPlayedGames: IUserGameInfo[]
   @Input({ required: true }) public user: IUser
   @Input({ required: true }) public recentPlayTime: number
   @Input({ transform: booleanAttribute }) public isFriend = false
 
-  @ViewChild('recentGamesCarousel', { static: false }) public carousel: Carousel | undefined
-  @ViewChild('recentGamesCont', { static: false }) public carouselCont: ElementRef | undefined
-
-  protected responsiveOptions: CarouselResponsiveOptions[] = []
+  protected responsiveOptions: CarouselResponsiveOptions[] = [
+    {
+      breakpoint: '1500',
+      numVisible: 3,
+      numScroll: 3
+    },
+    {
+      breakpoint: '950',
+      numVisible: 2,
+      numScroll: 2
+    },
+    {
+      breakpoint: '500',
+      numVisible: 1,
+      numScroll: 1
+    }
+  ]
 
   protected library: IUserGameInfo[] = []
   protected numVisible = 3
@@ -72,45 +60,10 @@ export class RecentGames implements OnInit, AfterViewInit, OnDestroy {
     this.state.setRecentGamesStatus(true)
   }
 
-  public ngAfterViewInit (): void {
-    if (this.carousel && this.carouselCont) {
-      this.resizeObserver = new ResizeObserver(() => {
-        this.updateCarouselConfig()
-      })
-      this.resizeObserver.observe(this.carouselCont.nativeElement)
-      this.updateCarouselConfig()
-    }
-  }
-
   public ngOnDestroy (): void {
     if (this.resizeObserver) {
       this.resizeObserver.disconnect()
     }
-  }
-
-  private updateCarouselConfig (): void {
-    if (!this.carousel || !this.carouselCont) {
-      return
-    }
-
-    const containerWidth = this.carouselCont.nativeElement.clientWidth
-
-    if (containerWidth < 950) {
-      this.numVisible = 1
-      this.numScroll = 1
-    }
-    else if (containerWidth < 1500) {
-      this.numVisible = 2
-      this.numScroll = 2
-    }
-    else {
-      this.numVisible = 3
-      this.numScroll = 3
-    }
-
-    this.carousel.numVisible = this.numVisible
-    this.carousel.numScroll = this.numScroll
-    this.cdr.detectChanges()
   }
 
   protected openGameDialog = (game: IUserGameInfo) => {
