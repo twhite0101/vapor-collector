fdc8fb2dfa382a853a04b36f6ba4aad51131e964
Travis White <travis.white0101@gmail.com>
2026-06-21 11:51:40 -0400

    Update production environment's apiUrl
    - Provide the new hosted Cloudflare URL for the API to the frontend's
      production environment object

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   domain: 'https://twhite0101.github.io/vapor-collector/',
-  apiUrl: ''
+  apiUrl: 'https://steam-api.travis-white0101.workers.dev'
 }
