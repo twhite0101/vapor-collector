e66af5a02e3e32d9193f825cc19657e0f6d68a22
Travis White <travis.white0101@gmail.com>
2026-06-25 17:51:07 -0400

    Update prod API URL

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   domain: 'https://twhite0101.github.io/vapor-collector/',
-  apiUrl: 'https://steam-api.travis-white0101.workers.dev'
+  apiUrl: 'https://vapor-collector-api.onrender.com'
 }
